5b913ce92b1c7bdcd85a5fa82cc51ce0988068f1
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-11-16 15:31:49 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
 int main() {
+  auto answer = 42;
+  auto question = "Life, The Universe, and Everything."s ;
+  
+  auto print = [](auto x) {
+		 std::cout << x;
+	       } ;
+  print(question);
+  print(answer);
+  std::cout << "\n";
+  
+  for(auto a = 1; a < 10; a++) {
+    for(auto b = 1; b < 10; b++)
+      std::cout << "\t" << a * b;
+    std::cout << "\n"s ;
+  }
+
   std::cout << "hello";
+
+  return 0;
 }
