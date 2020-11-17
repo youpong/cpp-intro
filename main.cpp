10e91531864ea53541e4ecc34de50c091571dfa4
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-11-17 22:12:56 +0900

    array

--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
-auto f = []() {
-  std::cout << 1 ;
- };
+auto f = []() { std::cout << 1; };
 
 int name_scope() {
-  f() ;
+  f();
 
-  auto f = []() {
-    std::cout << 2 ;
-  };
+  auto f = []() { std::cout << 2; };
 
-  f() ;
+  f();
 
   {
-    f() ;
-    auto f = []() {
-      std::cout << 3 ;
-    };
+    f();
+    auto f = []() { std::cout << 3; };
     f();
   }
 
@@ -24,27 +18,41 @@ int name_scope() {
   return 0;
 }
 
-int main() {
-
-  [](auto x){ std::cout << x; }("hello"s);
+void foo() {
+  [](auto x) { std::cout << x; }("hello"s);
 
   auto answer = 42;
-  auto question = "Life, The Universe, and Everything."s ;
-  
-  auto print = [](auto x) {
-		 std::cout << x;
-	       } ;
+  auto question = "Life, The Universe, and Everything."s;
+
+  auto print = [](auto x) { std::cout << x; };
   print(question);
   print(answer);
   std::cout << "\n";
-  
-  for(auto a = 1; a < 10; a++) {
-    for(auto b = 1; b < 10; b++)
+
+  for (auto a = 1; a < 10; a++) {
+    for (auto b = 1; b < 10; b++)
       std::cout << "\t" << a * b;
-    std::cout << "\n"s ;
+    std::cout << "\n"s;
   }
 
   std::cout << "hello";
+}
 
-  return 0;
+template <typename T, std::size_t N> struct array {
+  T storage[N];
+
+  T &operator[](std::size_t i) {
+    return storage[i];
+  }
+
+  void fill(T const &u) {
+    for (std::size_t i = 0; i != N; ++i) {
+      storage[i] = u;
+    }
+  }
+};
+
+struct array_iterator {};
+
+int main() {
 }
