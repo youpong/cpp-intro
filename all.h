21905cb80271d233e972bf46e6f241a1958679c7
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-01-09 12:09:04 +0900

    test.

--- all.h
+++ all.h
@@ -78,13 +78,13 @@ auto expect = [](auto line, auto expected, auto actual) {
 //   1: iterator class with index
 //   2: iterator class with pointer
 //   3: pointer as iterator
-#define ARRAY_ITERATOR 2
+#define ARRAY_ITERATOR 3
 
-// Result
+// Result                 S S S S S S
 //------------------------------------
 // ARRAY_ITERATOR 1       X     X
 //                2         X     X
 //                3           X     X
-// compiler       g++     X X X
-//                clang++       X X X
+// compiler       clang++ X X X
+//                g++           X X X
 //
