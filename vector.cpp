2c77a2e729f365f4c711ab2c7b29943de67fc618
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-02-04 20:33:53 +0900

    std::allocator_traits<Alloc>

--- vector.cpp
+++ vector.cpp
@@ -2,20 +2,24 @@
 
 static void test_allocator() {
   std::allocator<std::string> a;
+  using traits = std::allocator_traits<decltype(a)>;
 
 #ifndef QUIET_MODE
   // warning: ignoring return value of function declared with 'nodiscard'
   a.allocate(1);
 #endif
 
-  std::string *p = a.allocate(1);
+  std::string *p = traits::allocate(a, 1);
 
-  std::string *s = new (p) std::string("hello");
+  // vector.cpp:14:16: error: cannot initialize a variable of type
+  // 'std::string *' (aka 'basic_string<char> *') with an rvalue
+  // of type 'void'
+  std::string *s = traits::construct(a, p, "hello");
   expect(__LINE__, "hello"s, *s);
 
-  s->~basic_string();
+  traits::destroy(a, s);
 
-  a.deallocate(s, 1);
+  traits::deallocate(a, p, 1);
 }
 
 void test_all_vector() {
