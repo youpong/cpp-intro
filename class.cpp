ceb9ddb2ca639a3cf4d98f4b0b6003db79f3038b
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-02-05 12:57:09 +0900

    introduce cat.weight

--- class.cpp
+++ class.cpp
@@ -14,23 +14,31 @@ class Cat {
 public:
   void set_age(int age) { this->age = age; }
   int get_age() { return age; }
+  void set_weight(double weight) {
+    // can set before declare
+    this->weight = weight;
+  }
+  double get_weight() { return weight; }
 
 private:
-  std::string name;
+  double weight; // kg
 };
 
 static void test_access_modifier_class() {
   Cat cat;
   cat.set_age(77);
   expect(__LINE__, 77, cat.get_age());
+  cat.set_weight(7.8);
+  expect(__LINE__, 7.8, cat.get_weight());
 #ifdef ACCESS_MOD
   // error: 'age' is a private member of 'Cat'
   cat.age = 78;
+  cat.weight = 7.8;
 #endif
 }
 
 static void test_access_modifier_struct() {
-  //  Dog
+  //  Dog dog;
 }
 
 void test_all_class() {
