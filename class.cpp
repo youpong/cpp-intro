e317883ccebd10992c4ea2d2134cb33b9574af8b
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-02-06 14:29:16 +0900

    introduce dog

--- class.cpp
+++ class.cpp
@@ -3,12 +3,17 @@
 //#define ACCESS_MOD
 
 struct Dog {
-  int age;
+  // default access modifier is 'public'
+  Dog(int age) : age(age) {}
+
+  int get_age() { return age; }
 
 private:
+  int age;
 };
 
 class Cat {
+  // default access modifier is 'private'
   int age;
 
 public:
@@ -23,7 +28,6 @@ private:
 
 static void test_access_modifier_class() {
   Cat cat(77, 7.8);
-
   expect(__LINE__, 77, cat.get_age());
   expect(__LINE__, 7.8, cat.get_weight());
 #ifdef ACCESS_MOD
@@ -34,7 +38,12 @@ static void test_access_modifier_class() {
 }
 
 static void test_access_modifier_struct() {
-  //  Dog dog;
+  Dog dog(79);
+  expect(__LINE__, 79, dog.get_age());
+#ifdef ACCESS_MOD
+  // error: 'age' is a private member of 'Cat'
+  dog.age = 79;
+#endif
 }
 
 void test_all_class() {
