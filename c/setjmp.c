331f907b8589b4bbf94e7921319bd940c5c1d463
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-05-17 23:35:18 +0900

    change func_caller()

--- c/setjmp.c
+++ c/setjmp.c
@@ -1,12 +1,12 @@
 #include "util.h"
 #include <setjmp.h>
-#include <stdio.h>
+//#include <stdio.h>
 
 jmp_buf g_env;
 int g = 100;
 
 static void func() {
-  g *= 2;
+  g++;
   longjmp(g_env, 1);
 }
 
@@ -14,14 +14,19 @@ static void func_caller() {
   int x = 10;
   volatile int y = 20;
 
-  if (setjmp(g_env) == 0) {
-    y *= 2;
-    g *= 2;
-    func();
-  } else {
-    // longjmp()
-    printf("x == %d, y == %d, g == %d\n", x, y, g);
+  switch (setjmp(g_env)) {
+  case 0:
+    y++;
+    g++;
+    break;
+  case 1:
+    expect(__LINE__, 10, x);
+    expect(__LINE__, 21, y);
+    expect(__LINE__, 102, g);
+    return;
   }
+
+  func();
 }
 
 void test_all_setjmp() { func_caller(); }
