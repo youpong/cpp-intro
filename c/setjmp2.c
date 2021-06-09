e517d37efcdd459a839767810ce825c32314f9b1
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-06-09 16:38:38 +0900

    be more clear about seaquence.

--- c/setjmp2.c
+++ c/setjmp2.c
@@ -2,45 +2,41 @@
 #include <setjmp.h>
 #include <stdlib.h>
 
-// static int g = 56;
-
-static void func(jmp_buf jbuf, int val) {
-  //++g;
-  longjmp(jbuf, val);
-}
+static void func(jmp_buf jbuf, int val) { longjmp(jbuf, val); }
 
 static void func_caller() {
   jmp_buf env[2];
-  int flg = 0;
+  char trace[255];
+  char *p;
 
-  // clang-format off
-  // execute A->B->C->D->E
+  p = trace;
   switch (setjmp(env[0])) { // setjmp0
-  case 0:                   // A
-    flg |= 0b1; 
-    func(env[0], 1);        // goto setjmp0
+  case 0:
+    *p++ = 'A';
+    func(env[0], 1); // goto setjmp0
     break;
-  case 1:                   // B
-    flg |= 0b10;
+  case 1:
+    *p++ = 'B';
     break;
-  case 2:                   // D
-    flg |= 0b100;
-    func(env[1], 3);        // goto setjmp1
+  case 2:
+    *p++ = 'C';
+    func(env[1], 3); // goto setjmp1
     break;
   }
 
   switch (setjmp(env[1])) { // setjmp1
-  case 0:                   // C
-    flg |= 0b1000;
-    func(env[0], 2);        // goto setjmp0
+  case 0:
+    *p++ = 'D';
+    func(env[0], 2); // goto setjmp0
     break;
-  case 3:                   // E
-    flg |= 0b10000;
+  case 3:
+    *p++ = 'E';
     break;
   }
-  // clang-format on  
 
-  expect(__LINE__, flg, 0b11111);
+  *p = '\0';
+
+  expect_str(__LINE__, trace, "ABDCE");
 }
 
 void test_all_setjmp2() { func_caller(); }
